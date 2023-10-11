271ce23fef13f7ed4d3b4546657c2dae0e16f9ef
Nicolasolgoso <145667874+Nicolasolgoso@users.noreply.github.com>
2023-10-11 17:46:29 +0200

    Update moneda.py

--- moneda.py
+++ moneda.py
@@ -1,19 +1,27 @@
+#Pedimos al usuario que ingrese la cantidad en euros a convertir
 euros = float(input("Introduce la cantidad en euros que quieres convertir: "))
-cambio = int(input("Para convertirlo a dólares marca 1 a pesos 2, a yuanes 3, a libras 4 y a francos 5 : "))
+#Le preguntamos a que moneda pretende convertir los euros
+cambio = int(input("Para convertirlo a: \n dólares marca 1: \n  pesos marca 2: \n  yuanes marca 3: \n  libras marca 4: \n  francos marca 5 : "))
+#Si elige dolares realizamos la conversión a dólares
 if cambio == 1:
     conversion_dolares = euros * 0.94
     print(f"Equivale a {conversion_dolares} dólares")
+#Si elige pesos realizamos la conversión a pesos
 elif cambio == 2:
     conversion_pesos = euros * 19.25
     print(f"Equivale a {conversion_pesos} pesos")
+#Si elige yuanes realizamos la conversión a yuanes
 elif cambio == 3:
     conversion_yuanes = euros * 7.63
     print(f"Equivale a {conversion_yuanes} yuanes")
+#Si elige libras realizamos la conversión a libras
 elif cambio == 4:
     conversion_libras = euros * 1.15
     print(f"Equivale a {conversion_libras} libras")
+#Si elige francos realizamos la conversión a francos
 elif cambio == 5:
     conversion_francos = euros * 0.96
     print(f"Equivale a {conversion_francos} francos")
+#Si introduce algo distinto da error
 else:
-    print("Por favor marca un número del 1-5")
+    print("Error.Por favor marca un número del 1-5")
